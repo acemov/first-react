0a6bc6abffe4121a9d2d42a8dc762eeedb722979
Franco <aesevv@gmail.com>
2024-11-10 20:17:57 -0300

    implementando eventos del DOM

--- src/components/TiraPeliculas.js
+++ src/components/TiraPeliculas.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Pelicula from './Pelicula';
 
-function TiraPeliculas() {
-    return (
-        <div className="App">
-            <p>-----------------</p>
-            <Pelicula genero={["acción", "acentura"]} nombres="Logan" descripcion="muy buena :)" >
-                <p>- hacemos uso de children😎</p>
-            </Pelicula>
-            <Pelicula genero={["tegnologia", "errores", "ia"]} nombres="ERROR" descripcion="muy mala :(" >
-                <p>- childreeeen🤣</p>
-            </Pelicula>
-        </div >
-    );
+class TiraPeliculas extends Component {
+    constructor(props) {
+        super(props);
+        this.lanzarAlerta = this.lanzarAlerta.bind(this);
+        this.cambiarNegro = this.cambiarNegro.bind(this);
+        this.cambiarBlanco = this.cambiarBlanco.bind(this);
+    }
+
+    lanzarAlerta() {
+        alert("Hola, soy una alerta");
+    }
+
+    cambiarNegro() {
+        document.querySelector("body").style.backgroundColor = "black";
+    }
+
+    cambiarBlanco() {
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1 onClick={this.lanzarAlerta}>Que ondaaaaaa 😎</h1>
+                <p>-----------------</p>
+                <Pelicula genero={["acción", "acentura"]} nombres="Logan" descripcion="muy buena :)">
+                    <p>- hacemos uso de children😎</p>
+                </Pelicula>
+                <Pelicula genero={["tegnologia", "errores", "ia"]} nombres="ERROR" descripcion="muy mala :(">
+                    <p>- childreeeen🤣</p>
+                </Pelicula>
+            </div>
+        );
+    }
 }
 
 export default TiraPeliculas;
---
+++ test.js
@@ -0,0 +1,19 @@
+const { log } = require('console');
+const path = require('path');
+
+let primero = Date.now()
+let nombre = "Franco";
+let franse = `Hola Sr ${nombre}`
+let segundos = Date.now()
+console.log(primero);
+console.log(franse);
+console.log(segundos);
+console.log("--------------------");
+console.log(path.extname(__filename));
+console.log("--------------------");
+let uno = 1;
+let uno2 = 1
+console.log(uno === uno2);
+console.log("--------------------");
+console.log(__filename);
+console.log(2 + 2);
